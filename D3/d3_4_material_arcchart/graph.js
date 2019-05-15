d312101587c54f4ea76cdafc618355d7040bde59
ramlack <jiaxiaole_2009@163.com>
2019-05-15 23:47:22 +0800

    0515

---
+++ D3/d3_4_material_arcchart/graph.js
@@ -0,0 +1,90 @@
+const dims = {
+    height: 300,
+    width: 300,
+    radius: 150
+}
+
+const cent = {
+    x: (dims.width / 2 + 5),
+    y: (dims.height / 2 + 5)
+}
+
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', dims.width + 150)
+    .attr('height', dims.height + 150)
+
+const graph = svg.append('g')
+    .attr('transform', `translate(${cent.x},${cent.y})`)
+
+const pie = d3.pie()
+    .sort(null)
+    .value(d => d.cost
+    )
+
+const angles = pie([
+    {
+        name: 'rent',
+        cost: 500
+    },
+    {
+        name: 'bill',
+        cost: 400
+    },
+    {
+        name: 'gaming',
+        cost: 200
+    }
+])
+console.log(angles)
+
+const arcPath = d3.arc()
+    .outerRadius(dims.radius)
+    .innerRadius(dims.radius / 2)
+console.log(arcPath(angles[0]))
+
+// update function
+const update=(data)=>{
+    console.log(data)
+    const paths = graph.selectAll('path')
+    .data(pie(data))
+    console.log(paths)
+
+
+    paths.enter()
+    .append('path')
+    .attr('class','arc')
+    .attr('d',arcPath)
+    .attr('stroke','#fff')
+    .attr('stroke-width',3)
+
+}
+
+var data = [];
+db.collection("expenses").onSnapshot(
+    res => {
+        console.log(res)
+        console.log(res.docChanges())
+        res.docChanges().forEach(change => {
+            const doc = {
+                ...change.doc.data(), id: change.doc.id
+            }
+            switch (change.type) {
+                case 'added':
+                    data.push(doc);
+                    break;
+                case 'modified':
+                    const index = data.findIndex(item => item.id == doc.id);
+                    data[index] = doc;
+                    break;
+                case 'removed':
+                    data = data.filter(item => item.id !== doc.id);
+                    break;
+                default:
+                    break;
+            }
+
+        });
+        update(data)
+    }
+)
