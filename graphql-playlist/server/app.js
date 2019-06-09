66dc833a5d1088e4c981fa81adf7dcb87d7eaada
jiaxiaole <jiaxiaole_2009@163.com>
2019-06-09 10:58:54 +0800

    graphql

---
+++ graphql-playlist/server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require("./schema/schema")
+const app = express();
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://jiaxiaole:YEw_ZajZZwyg5aM@cluster0-klii5.mongodb.net/test")
+mongoose.connection.once('open',()=>{
+    console.log('connneted to db')
+})
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    // pass in a schema property
+    schema,
+    graphiql:true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
